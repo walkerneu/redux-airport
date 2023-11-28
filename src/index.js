6974c91495b3a68b5578c004711aedc78ee423ed
Walker Neudorff <walkerneudorff@gmail.com>
2023-11-28 14:47:41 -0600

    Set up Store in index.js, added reducer, sourced in redux

--- src/index.js
+++ src/index.js
@@ -2,20 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
 
-/** TODO: import REDUX **/
 
+/** TODO: import REDUX **/
+import {createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 
 /** TODO: Add REDUCERS */
 
+const airlineCount = (state=[], action) => {
+    return state
+}
 
 /** TODO: Create store */
+
+const airlineStoreDutyFree = createStore(
+    combineReducers({
+        airlineCount
+    })
+);
 
 
 
 // Be sure to add the Provider! Just wrap App with it. Don't copy and paste from lecture, that will cause issues.
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
+    <Provider store={airlineStoreDutyFree}>
         <App />
-    </React.StrictMode>
+    </Provider>
 );
